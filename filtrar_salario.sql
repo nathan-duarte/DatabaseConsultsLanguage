1051719039d7c8f388629f9872197880a2be29c7
Nathan Duarte da Silva <70297725+nathan-duarte@users.noreply.github.com>
2024-11-26 22:47:31 -0300

    Add files via upload

---
+++ filtrar_salario.sql
@@ -0,0 +1,5 @@
+SELECT last_name, salary
+FROM employees
+WHERE salary >= 1000;
+
+-- Vai selecionar os salários que são maior ou igual a 1000.
---
+++ filtrar_salarioII.sql
@@ -0,0 +1,5 @@
+SELECT last_name, salary
+FROM employees
+WHERE salary BETWEEN 10000 AND 15000;
+
+-- Vai selecionar os salários que estão entre 10000 e 15000.
